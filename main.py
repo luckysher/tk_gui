a7d3ba470a0b53be8ed977def364f9d214b226fe
luckysher <luckyshera95@gmail.com>
2018-01-15 12:32:44 +0530

    added comments for main file

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+#
+# Main file running TK GUI
+#
 
+# import for form class
 from form import Form
 
 
 if __name__ == '__main__':
+    # Creating object of Form class
     form = Form()
+
+    # Run method for running form
     form.run()
