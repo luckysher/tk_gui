38aded5a29659b0a4f6d743c93644864e6a401ec
luckysher <luckyshera95@gmail.com>
2018-01-15 12:31:55 +0530

    added comments for form class

--- form.py
+++ form.py
@@ -1,20 +1,30 @@
+#################################################################
+#                                                               #
+# Form class for showing form with multiple components          #
+#                                                               #
+#################################################################
 
-
+# imports for various components from TK
 from Tkinter import *
 
-
+# Form class
 class Form(Tk):
+
+    # form class constructor
     def __init__(self):
         self.tk = Tk()
         print("Form  class")
 
+    # frame for handling various components
     def frame(self):
-        frm = Frame(self, width=60, height=50, bg="green")
+        frm = Frame(self, width=900, height=500, bg="green")
         frm.pack()
 
+    # method for arranging various component
     def arangeComponents(self):
         self.frame()
 
+    # Run method for running form
     def run(self):
         self.arangeComponents()
         self.mainloop()
