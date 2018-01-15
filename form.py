699ed4d171cf9f0884eefb72dab29f72abbcc269
luckysher <luckyshera95@gmail.com>
2018-01-15 12:20:30 +0530

    added method for generating fomm

--- form.py
+++ form.py
@@ -8,6 +8,9 @@ class Form(Tk):
         self.tk = Tk()
         print("Form  class")
 
+    def frame(self):
+        frm = Frame(self, width=60, height=50, bg="green")
+        frm.pack()
 
     def arangeComponents(self):
         self.frame()
