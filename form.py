890de230c9930a11ece00483703077d793bf7572
luckysher <luckyshera95@gmail.com>
2018-01-22 20:35:37 +0530

    adding comments for form methods

--- form.py
+++ form.py
@@ -23,14 +23,17 @@ class Form(Tk):
         frm.pack(fill=BOTH)
         return frm
 
+    # Method for adding text field
     def addTextField(self, master, col, row):
         tfName = Entry(master, fg="black")
         tfName.config(width=20, font="Sans-serif, 30")
 
+    # Method for adding label
     def addLabel(self, master, col, row, text):
         lblName = Label(master, text=text, fg="black")
         lblName.config(width=10, height=2, font="Sans-serif, 17")
 
+    # Method for adding heading
     def formHeading(self, master, row, heading_text):
         heading = Label(master, text=heading_text, fg="black")
         heading.config(width=20, height=2, font="Sans-serif, 19")
